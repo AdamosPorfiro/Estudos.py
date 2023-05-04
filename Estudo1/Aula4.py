72971917ad2cbd8b2d5462ce06194cf2414eb86b
Adamos Porfiro <adamos.contato@gmail.com>
2023-05-04 14:59:40 -0300

    Projeto1.py

--- Estudo1/Aula4.py
+++ Estudo1/Aula4.py
@@ -63,3 +63,4 @@ for idade in idades: # Laço chado de idade que invoca a variavel idades
 print(total) # Exibi na tela para o usuario o total somado (Resultado).
 
 
+
---
+++ Estudo1/Projeto1.py
@@ -0,0 +1,25 @@
+'''
+Fatorial de um numero
+
+Crie um programa que vai receber um numero e vai exibir o fatorial de uma numero
+
+# Utilize o metodo dos 5q's;
+# Analise criticamente o problma e descubra;
+# Tente explicar o problema para você mesmo em voz alta e peça mais informações / 
+investigue mais até você entender completamente o programa.
+
+1 - Quais são os dados de entrada necessarios?
+
+
+2 - O que devo fazer com estes dados?
+
+
+3 - Quais são as restrições desse problema?
+
+
+4 - Qual é o resultado esperado?
+
+
+5 - Quais são as sequencias de passos a serem feitas para se alcançar o resultado esperado?
+
+'''
