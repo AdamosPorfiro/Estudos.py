100586c31dd6ac351984fc0d472ed7cdedd334eb
Adamos Porfiro <adamos.contato@gmail.com>
2023-05-04 18:12:29 -0300

    Projeto_2

---
+++ Estudo1/Projeto2.py
@@ -0,0 +1,20 @@
+# Projeto 2 - Chute um numero
+
+'''
+Escreve um programa que ao iniciar el vai gerar um numero aleatório de 1 a 10 que permita o usuario cgutar um numero até que
+o valor gerado no inicio do programa seja chutado corretamente.
+
+O programa deve informa se o chute foi, acima, abaixo ou igual ao valor aleatório gerado no inicio do programa
+
+# Utilize o metodo dos 5q's;
+# Analise criticamente o problma e descubra;
+# Tente explicar o problema para você mesmo em voz alta e peça mais informações / 
+investigue mais até você entender completamente o programa.
+
+1- Quais são os dados necessarios?
+2- O que devo fazer com esses dados?
+3- Quais são as restrições desse problema?
+4- Qual é o resultado esperado?
+5 - Quais são os passos a serem feitos para chegar ao resultado esperado?
+ 
+'''
